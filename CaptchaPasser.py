ad8cfc50be10a544eedbe4769deda9c1c5507a61
Xiaoyu <732004709@qq.com>
2023-02-27 18:28:26 +0800

    优化识别逻辑，增加requirements

--- CaptchaPasser.py
+++ CaptchaPasser.py
@@ -4,27 +4,33 @@ from playwright.sync_api import sync_playwright, Route
 from CaptchaCv2 import (get_track_list, qq_mark_pos)
 
 distance = 0
+is_reflashed_img = False
 img = "bg.png"
+retryTimes = 10
+
 
 def handle_captcha(route: Route) -> None:
     response = route.fetch()
     if response.status == 200:
-        print("拦截到刷新验证码请求")
         buffer = response.body()
         # 下载指定规则url的验证码图片
         if "index=1" in response.url:
+            is_reflashed_img = True
             with open(img, "wb") as f:
                 f.write(buffer)
     route.continue_()
 
+
 def dragbox_location():
     for i in range(5):
-        dragbox_bounding = page.frame_locator("#tcaptcha_iframe").locator("#tcaptcha_drag_thumb").bounding_box()
-        if dragbox_bounding is not None and dragbox_bounding["x"]>20:
+        dragbox_bounding = page.frame_locator("#tcaptcha_iframe").locator(
+            "#tcaptcha_drag_thumb").bounding_box()
+        if dragbox_bounding is not None and dragbox_bounding["x"] > 20:
             return dragbox_bounding
     return None
 
-def drag_to_breach():
+
+def drag_to_breach(move_distance):
     drag_box = dragbox_location()
     if drag_box is None:
         return False
@@ -35,16 +41,29 @@ def drag_to_breach():
     for i in move_distance:
         location_x += i
         page.mouse.move(location_x, drag_box["y"])
-        page.wait_for_timeout(50)
     page.mouse.up()
-    if page.get_by_text("后重试") is not None and page.get_by_text("获取验证码") is None: 
-        print("识别成功")  
+    if page.get_by_text("后重试") is not None:
+        print("识别成功")
         return True
     return False
 
+
+def calc_distance():
+    for i in range(retryTimes):
+        print(f"识别验证码距离中，当前等待轮数{i+1}/{retryTimes}")
+        try:
+            res = qq_mark_pos(img)
+            distance = res.x.values[0]
+            if distance > 0:
+                print(f"获取到缺口距离：{distance}")
+                return distance
+        except Exception as e:
+            print(f"识别错误, 异常：{e}")
+
+
 with sync_playwright() as p:
     #browser = p.chromium.launch(channel="msedge",proxy={"server": "http://{}".format(proxy)})
-    browser = p.chromium.launch(channel="msedge",headless=False)
+    browser = p.chromium.launch(channel="msedge", headless=False)
     iphone_12 = p.devices["iPhone 12"]
     context = browser.new_context(
         record_video_dir="videos/",
@@ -53,40 +72,30 @@ with sync_playwright() as p:
     page = context.new_page()
     # 下载指定规则的验证码图片
     page.route("**/t.captcha.qq.com/hycdn**", handle_captcha)
+    page.route("**/t.captcha.qq.com/cap_union_new_getcapbysig**", handle_captcha)
     page.goto(
         "https://wap.showstart.com/pages/passport/login/login?redirect=%252Fpages%252FmyHome%252FmyHome")
-    page.fill("uni-view.form-phone > uni-input > div > input", "12345104596")
-    page.locator("uni-view:nth-child(2) > uni-view.btn").click()
 
-    print("downloading captcha...")
+    page.get_by_role("spinbutton").fill("13265104596")
+    page.get_by_text("获取验证码").click()
 
-    retryTimes = 10
+    move_distance = None
     for i in range(retryTimes):
-        print(f"识别验证码距离中，当前等待轮数{i}/{retryTimes}")
-        try:
-            res = qq_mark_pos(img)
-            distance = res.x.values[0]
-            if distance > 0:
-                print(f"获取到缺口距离：{distance}")
-                break
-        except Exception as e:
-            print(f"识别错误, 异常：{e}")
+        print(f"拖动滑块中，当前尝试轮数{i+1}/{retryTimes}")
+        # 验证码刷新 重新计算距离
+        if is_reflashed_img or move_distance is None:
+            distance = calc_distance()
+            page.wait_for_timeout(200)
 
-        page.wait_for_timeout(500)
+            true_distance = distance * 353 / 680
+            move_distance = get_track_list(true_distance)
+            print(f"获取到相对滑动距离{true_distance}, 模拟拖动列表{move_distance}")
+            is_reflashed_img = False
 
-    if distance <= 0:
-        print("识别失败，退出程序")
-    else:
-        # 通过页面上验证码框的宽高，计算相对位置
-        true_distance = distance * 353 / 680
-        move_distance = get_track_list(true_distance)    
-        print(f"获取到相对滑动距离{true_distance}, 模拟拖动列表{move_distance}")
+        drag_result = drag_to_breach(move_distance)
+        if drag_result:
+            break
 
-        for i in range(retryTimes):
-            print(f"拖动滑块中，当前尝试轮数{i}/{retryTimes}") 
-            drag_result = drag_to_breach()
-            if drag_result:
-                break;
-    
     print("识别结束，退出程序")
+
     browser.close()
